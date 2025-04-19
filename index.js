7a7801bf7c3edc20b859eb1149163d7a36501bc3
chaitanya732 <95459967+chaitanya732@users.noreply.github.com>
2025-04-19 16:55:00 +0530

    Add  dependency

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+import express, { json } from 'express';
+import cors from 'cors';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
-const authRoutes = require('./routes/auth');
+import authRoutes from './routes/auth';
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
