b7018ad79fcefffb7f18b5e27cad09a2306690f5
chaitanya732 <95459967+chaitanya732@users.noreply.github.com>
2025-04-19 17:06:07 +0530

    Fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
